b4866f829d267f147ea257ea89532be98471e685
Christofer Sousa <christofersousa.js@gmail.com>
2020-09-21 16:36:46 -0300

    Update script.js

--- files/script.js
+++ files/script.js
@@ -65,8 +65,26 @@ function generatePassword(){
 function generateX(){
     
     const xs = [];
+
     if (upperEl.checked){
         xs.push(getUppercase());
     }
+
+    if (lowerEl.checked){
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked){
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked){
+        xs.push(getSymbol());
+    }
+
+    if (xs.length === 0) return '';
+
+    return xs[Math.floor(Math.random() * xs.length)];
+    
 }
 
